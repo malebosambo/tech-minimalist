fbba60dcd043a74a6643c9d2cffa437dce69df77
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-09-25 04:16:52 +0200

    Update navigation.js

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -9,12 +9,12 @@ export default function Navigation() {
  
   return (
     <>
-      <Link href="/"><a className={paths[0] == router.pathname ? activeLink : inactiveLink}>Home</a></Link>
-      <Link href="/blog"><a className={paths[1] == router.pathname ? activeLink : inactiveLink}>Blog</a></Link>
-      <Link href="/podcast"><a className={paths[2] == router.pathname ? activeLink : inactiveLink}>Podcast</a></Link>
-      <Link href="/team"><a className={paths[3] == router.pathname ? activeLink : inactiveLink}>Team</a></Link>
-      <Link href="/resources"><a>Resources</a></Link>
-      <Link href="/contacts"><a className={paths[4] == router.pathname ? activeLink : inactiveLink}>Contacts</a></Link>
+      <Link href="/" className={ paths[0] == router.pathname ? activeLink : inactiveLink }>Home</Link>
+      <Link href="/blog" className={ paths[1] == router.pathname ? "activeLink" : "inactiveLink" }>Blog</Link>
+      <Link href="/podcast" className={ paths[2] == router.pathname ? "activeLink" : "inactiveLink" }>Podcast</Link>
+      <Link href="/team" className={ paths[3] == router.pathname ? "activeLink" : "inactiveLink" }>Team</Link>
+      <Link href="/resources">Resources</Link>
+      <Link href="/contacts" className={ paths[4] == router.pathname ? "activeLink" : "inactiveLink" }>Contacts</Link>
     </>
   );
 }
