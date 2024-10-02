ce5a8ba5c929a15391a65cec741162f1d4d3caf1
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-10-02 07:19:12 +0200

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -3,6 +3,8 @@ import BlogArticle from '../components/blogArticle';
 
 export default function Blog() {
   
+  const articles = getDatabase();
+
   return (
     <main className="BlogList">
       <div><h1>Blog Articles</h1></div>
