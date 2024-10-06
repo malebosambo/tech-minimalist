eed05ae78aaa231b3487730b4b4f773d3ea31b7d
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-10-06 08:38:28 +0200

    Update header.js

--- app/components/header.js
+++ app/components/header.js
@@ -17,12 +17,12 @@ export default function header() {
         <div>
           <Link href="/"><Image src="/Tech-Minimalist-Logo.jpeg" alt="Tech Minimalist Logo" height={50} width={50} /></Link>
         </div>
-        <div className="Mobile-Menu" style={{padding: "20px"}}>
+        <div className="Mobile_Menu" style={{padding: "20px"}}>
           <button onClick={showMenu} className={ !mobileMenu ? "" : "Menu" }>=</button>
           <button onClick={showMenu} className={ mobileMenu ? "" : "Menu" }>X</button>
         </div>
       </div>  
-      <div className={ mobileMenu ? "Menu-Visible" : "Menu" }>
+      <div className={ mobileMenu ? "Menu_Visible" : "Menu" }>
         <Navigation />
       </div>
     </div>
